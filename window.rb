199936142512cedbcb34d0941d7f8ed0390eb8bb
Serge Hänni <serge.haenni@simplificator.com>
2018-11-24 17:50:32 +0100

    Added basic map.

--- window.rb
+++ window.rb
@@ -10,8 +10,12 @@ class OfficeWarsWindow < Gosu::Window
     super(@width, @height)
     self.caption = 'Office Wars'
 
-
     @ui = Ui.new(self, @width, @height)
+    @state = :menu
+
+    @stone = Gosu::Image.new('assets/tileStone.png')
+    @stone_alternate = Gosu::Image.new('assets/tileRock.png')
+    @sand = Gosu::Image.new('assets/tileDirt.png')
   end
 
   def needs_cursor?; true; end
@@ -23,9 +27,14 @@ class OfficeWarsWindow < Gosu::Window
   def draw
     draw_background
 
-    @ui.vertical do |layout|
-      layout.button('Practice', text_offset: 5)
-      layout.button('Exit', text_offset: 55)
+    case @state
+    when :menu
+      @ui.vertical do |layout|
+        layout.button('Practice', text_offset: 5)
+        layout.button('Exit', text_offset: 55)
+      end
+    when :practice
+      draw_map
     end
   end
 
@@ -35,7 +44,7 @@ class OfficeWarsWindow < Gosu::Window
       @ui.clicked(mouse_x, mouse_y) do |text|
         case text
         when 'Practice'
-          puts 'starting game'
+          @state = :practice
         when 'Exit'
           exit
         end
@@ -49,6 +58,23 @@ class OfficeWarsWindow < Gosu::Window
 
   def draw_background
     draw_rect(0, 0, @width, @height, Gosu::Color.new(238, 231, 205))
+  end
+
+  def draw_map
+    width = 65
+    height = 89
+    vertical_offset = 50
+
+    4.times do |row|
+      4.times do |time|
+        @stone.draw(50 + time * 2 * width, 50 + row * height, 1)
+        @stone_alternate.draw(50 + time * 2 * width + width, 50 + row * height, 1)
+      end
+      4.times do |time|
+        @stone.draw(50 + width / 2 + time * 2 * width, 50 + row * height + vertical_offset, 1)
+        @sand.draw(50 + width / 2 + time * 2 * width + width, 50 + row * height + vertical_offset, 1)
+      end
+    end
   end
 end
 
